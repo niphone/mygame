a98edd867005667b26726f53a25c2547b064a913
niphone <niphone2011@gmail.com>
2024-10-01 15:59:05 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -1,61 +1,35 @@
 #!/bin/sh
 ##
 
+UUID=fb9f5f54-e951-11ec-ac4f-020017000b7b
+
 # Set ARG
 ARCH="64"
-DOWNLOAD_PATH="/tmp/v2ray"
+DOWNLOAD_PATH="/tmp/xray"
+mkdir -p ${DOWNLOAD_PATH} /etc/xray /usr/local/xray /var/log/xray
 
-mkdir -p ${DOWNLOAD_PATH}
-cd ${DOWNLOAD_PATH} || exit
+TAG=$(wget --no-check-certificate -qO- https://api.github.com/repos/xtls/Xray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
-TAG=$(wget --no-check-certificate -qO- https://api.github.com/repos/v2fly/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)
-if [ -z "${TAG}" ]; then
-    echo "Error: Get v2ray latest version failed" && exit 1
-fi
-echo "The v2ray latest version: ${TAG}"
+echo "The xray latest version: ${TAG}"
 
 # Download files
-V2RAY_FILE="v2ray-linux-${ARCH}.zip"
-DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
-echo "Downloading binary file: ${V2RAY_FILE}"
-echo "Downloading binary file: ${DGST_FILE}"
-
-# TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
-# wget -O ${DOWNLOAD_PATH}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} >/dev/null 2>&1
-# wget -O ${DOWNLOAD_PATH}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} >/dev/null 2>&1
-
-wget -O ${DOWNLOAD_PATH}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/${V2RAY_FILE} >/dev/null 2>&1
-wget -O ${DOWNLOAD_PATH}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/${DGST_FILE} >/dev/null 2>&1
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to download binary file: ${V2RAY_FILE} ${DGST_FILE}" && exit 1
-fi
-echo "Download binary file: ${V2RAY_FILE} ${DGST_FILE} completed"
-
-# Check SHA512
-LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
-STR=$(cat < v2ray.zip.dgst | grep 'SHA512' | head -n1)
-
-if [ "${LOCAL}" = "${STR}" ]; then
-    echo " Check passed" && rm -fv v2ray.zip.dgst
-else
-    echo " Check have not passed yet " && exit 1
-fi
+XRAY_FILE="Xray-linux-${ARCH}.zip"
+echo "Downloading binary file: ${XRAY_FILE}"
+wget -O ${DOWNLOAD_PATH}/xray.zip https://github.com/XTLS/Xray-core/releases/download/${TAG}/${XRAY_FILE} >/dev/null 2>&1
+echo "Download binary file: ${XRAY_FILE} completed"
 
 # Prepare
 echo "Prepare to use"
-unzip v2ray.zip && chmod +x v2ray v2ctl
-mv v2ray v2ctl /usr/bin/
-mv geosite.dat geoip.dat /usr/local/share/v2ray/
-# mv config.json /etc/v2ray/config.json
+unzip -d /usr/local/xray ${DOWNLOAD_PATH}/xray.zip
+chmod +x /usr/local/xray/xray
 
 # Set config file
-cat <<EOF >/etc/v2ray/config.json
+cat <<EOF >/etc/xray/config.json
 {
-    "log": {
-        "loglevel": "warning"
-    },
-     "inbounds": [
+  "log": {
+    "loglevel": "warning"
+  },
+  "inbounds": [
     {
       "listen": "::",
       "port": 8000,
@@ -63,7 +37,7 @@ cat <<EOF >/etc/v2ray/config.json
       "settings": {
         "clients": [
           {
-            "id": "fb9f5f54-e951-11ec-ac4f-020017000b7b",
+            "id": "${UUID}",
             "alterId": 0
           }
         ]
@@ -82,7 +56,7 @@ cat <<EOF >/etc/v2ray/config.json
       "settings": {
         "clients": [
           {
-            "id": "fb9f5f54-e951-11ec-ac4f-020017000b7b"
+            "id": "${UUID}"
           }
         ],
         "decryption": "none"
@@ -96,24 +70,19 @@ cat <<EOF >/etc/v2ray/config.json
       }
     }
   ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+  "outbounds": [
+    {
+      "tag": "direct",
+      "protocol": "freedom"
+    },
+    {
+      "tag": "block",
+      "protocol": "blackhole"
+    }
+  ]
 }
 EOF
 
-# Clean
-cd ~ || return
-rm -rf ${DOWNLOAD_PATH:?}/*
-echo "Install done"
-
-echo "--------------------------------"
-echo "Fly App Name: ${FLY_APP_NAME}"
-echo "Fly App Region: ${FLY_REGION}"
-echo "V2Ray UUID: ${UUID}"
-echo "--------------------------------"
-
-# Run v2ray
-/usr/bin/v2ray -config /etc/v2ray/config.json
+echo "XRay UUID: ${UUID}"
+# Run vxray
+/usr/local/xray/xray -c /etc/xray/config.json
